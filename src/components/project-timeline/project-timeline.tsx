a7ba662a47909d61625b1da9e435c837181de5b0
Alex <alex031994@mail.ru>
2024-12-24 09:52:56 +0300

    fix project time line

--- src/components/project-timeline/project-timeline.tsx
+++ src/components/project-timeline/project-timeline.tsx
@@ -15,62 +15,74 @@ import img8 from "../../assets/project-timeline/img8.png";
 import img9 from "../../assets/project-timeline/img9.png";
 import img10 from "../../assets/project-timeline/img10.png";
 import img11 from "../../assets/project-timeline/img11.png";
+import { useLocalization } from "../../context/useLocalization";
 
 const slidesEls = [
   {
     img: img1,
     text: "A legend tells that a farmer who passed by the Lake Pigo with his donkey suffering from eczema, cured his animal due to the beneficial effect of the waters, which made thesewaters famous in the region for centuries.",
     date: 1200,
+    textIt: "Una leggenda narra che un contadino, passando vicino al Lago Pigo con il suo asino affetto da eczema, guarì l’animale grazie all’effetto benefico delle acque, che divennero famose nella regione per secoli."
   },
   {
     img: img2,
     text: "A historic peace was signed between the two factions on Pigna's Lago Pigo Bridge, next to Pigna's sulphur thermal springs, which became the dividing line between the territories of Provenza and the Republic of Genoa. Nowadays, the Grand Hotel Pigna is situated right next to the historic bridge.",
     date: 1365,
+    textIt: "Una storica pace fu firmata tra due fazioni sul ponte del Lago Pigo a Pigna, accanto alle sorgenti sulfuree, che divennero la linea di confine tra i territori della Provenza e della Repubblica di Genova. Oggi, il Grand Hotel Pigna è situato proprio accanto allo storico ponte."
   },
   {
     img: img3,
-    text: "A legend tells that a farmer who passed by the Lake Pigo with his donkey suffering from eczema, cured his animal due to the beneficial effect of the waters, which made thesewaters famous in the region for centuries.",
+    text: "The chemical analysis of water was carried out, stating that it contains sulphur and probably hydrosulfite, carbonic acid, soda sulphate, soda carbonate, sodium chloride, traces of magnesium carbonate, and an organic nitrogenous substance.",
     date: 1839,
+    textIt: ""
   },
   {
     img: img4,
-    text: "A historic peace was signed between the two factions on Pigna's Lago Pigo Bridge, next to Pigna's sulphur thermal springs, which became the dividing line between the territories of Provenza and the Republic of Genoa. Nowadays, the Grand Hotel Pigna is situated right next to the historic bridge.",
+    text: "Commercial exploitation of the beneficial thermal waters began. Aroung this time, Claude Monet visited the neighborhood of Pigna - the small hamlet of Dolceacqua, he was so awed by this medieval bridge, he enthused that the structure was ‘a pearl of lightness’ and depicted the bridge in the painting “The Bridge at Dolceacqua”.",
     date: 1884,
+    textIt: ""
   },
   {
     img: img5,
-    text: "A legend tells that a farmer who passed by the Lake Pigo with his donkey suffering from eczema, cured his animal due to the beneficial effect of the waters, which made thesewaters famous in the region for centuries.",
+    text: "The very first real bathhouse was opened in Pigna. The water was taken from a well presumably with buckets which, tied to a rope, were lowered along a natural cavity reinforced inside by bricks and raised to a height of about twenty metres.",
     date: 1903,
+    textIt: ""
   },
   {
     img: img6,
-    text: "A historic peace was signed between the two factions on Pigna's Lago Pigo Bridge, next to Pigna's sulphur thermal springs, which became the dividing line between the territories of Provenza and the Republic of Genoa. Nowadays, the Grand Hotel Pigna is situated right next to the historic bridge.",
+    text: "After the Great War, the lower part of the mill complexes was almost entirely destroyed. Only after the Second World War, Giovanni Battista Manesero, owner of the spring, revived the idea of properly utilising the area’s sulphurous waters.",
     date: 1920,
+    textIt: ""
   },
   {
     img: img7,
-    text: "A legend tells that a farmer who passed by the Lake Pigo with his donkey suffering from eczema, cured his animal due to the beneficial effect of the waters, which made thesewaters famous in the region for centuries.",
+    text: "The first thermal spa in Pigna was built — the rooms with tubs, in which it was possible to bathe in the sulphurous water. There were about 3,000 Italian and foreign tourists, mostly French, visiting the spa complex at that time. Among the famous customers were the tenor Di Stefano and Princess Grace. The sulphurous water baths operated at full capacity until 1989.",
     date: 1954,
+    textIt: ""
   },
   {
     img: img8,
-    text: "A historic peace was signed between the two factions on Pigna's Lago Pigo Bridge, next to Pigna's sulphur thermal springs, which became the dividing line between the territories of Provenza and the Republic of Genoa. Nowadays, the Grand Hotel Pigna is situated right next to the historic bridge.",
+    text: "The modern thermal spa hotel was built and operated as a 4* Grand Hotel Antiche Terme di Pigna. ",
     date: 2000,
+    textIt: ""
   },
   {
     img: img9,
-    text: "A legend tells that a farmer who passed by the Lake Pigo with his donkey suffering from eczema, cured his animal due to the beneficial effect of the waters, which made thesewaters famous in the region for centuries.",
+    text: "After the change of the hotel ownership, a new concept of the luxury 5* thermal wellness retreat Grand Hotel Pigna was created.",
     date: 2020,
+    textIt: ""
   },
   {
     img: img10,
-    text: "A historic peace was signed between the two factions on Pigna's Lago Pigo Bridge, next to Pigna's sulphur thermal springs, which became the dividing line between the territories of Provenza and the Republic of Genoa. Nowadays, the Grand Hotel Pigna is situated right next to the historic bridge.",
+    text: "An exquisite concept of hotel’s interior design and renovation of the Grand Hotel Pigna was created by Alex Kravetz",
     date: 2022,
+    textIt: ""
   },
   {
     img: img11,
-    text: "A legend tells that a farmer who passed by the Lake Pigo with his donkey suffering from eczema, cured his animal due to the beneficial effect of the waters, which made thesewaters famous in the region for centuries.",
+    text: "The approval documentation, research and planning work have been completed. The project is at the stage of raising investment for the implementation of the renovation plans. ",
     date: 2024,
+    textIt: ""
   },
 ];
 
@@ -87,14 +99,17 @@ const textItems = [
 
 
 const ProjectTimeline = () => {
+
+
+  const { locale } = useLocalization()
   return (
     <div className="w-full lg:pt-44 sm:pt-20 bg-loading-color lg:pb-48 sm:pb-20 px-[18px]" id="project-timeline">
       <div className="w-full flex justify-center items-center flex-col gap-4 text-main-white">
         <h2 className="lg:text-4xl sm:text-2xl font-patrizia font-normal uppercase">
-          Project Timeline
+          {locale === "ENG" ? "Project Timeline" : "Cronologia del Progetto"}
         </h2>
         <span className="lg:text-xl sm:text-xs font-helvetica font-normal opacity-80 text-center">
-          From mediaeval past to a premium wellness destination of the future.
+          {locale === "ENG" ? "From mediaeval past to a premium wellness destination of the future." : "Dal passato medievale a una destinazione benessere premium del futuro."}
         </span>
       </div>
       <div className="w-full flex justify-center items-center">
